d03073d986779aaceace5a41100235f0a73caf7c
mohammad ahmadi <myprojectsfile@hotmail.com>
2018-01-12 23:49:47 +0330

    پس از تکمیل نسخه اولیه وب سایت

--- src/app/invest/perspective/perspective.component.ts
+++ src/app/invest/perspective/perspective.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SafeHtml } from '@angular/platform-browser/src/security/dom_sanitization_service';
 
 @Component({
   selector: 'app-perspective',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerspectiveComponent implements OnInit {
 
-  constructor() { }
+  sanHtml:SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.sanHtml = sanitizer.bypassSecurityTrustHtml('<div id="15156983941068756"><script type="text/JavaScript" src="https://www.aparat.com/embed/c1Gpr data[rnddiv]=15156983941068756&data[responsive]=yes"></script></div>');
+  }
 
   ngOnInit() {
   }
